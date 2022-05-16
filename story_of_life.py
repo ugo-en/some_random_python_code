d35701b58f2e4794b18dda4eb969bda03bb3d87e
ugo-en <ugochukwuenwachukwu@gmail.com>
2022-05-16 16:46:27 +0100

    First main commit, added story_of_life.py and virtual environment

---
+++ story_of_life.py
@@ -0,0 +1,14 @@
+from all_necessities import *
+
+"""
+This is a simple guideline on how to live life
+"""
+
+def live(current_state):
+    if current_state == "awesome":
+        stay_awesome()
+    elif current_state == "meh":
+        make_life_interesting()
+    else:
+        be_awesome()
+
